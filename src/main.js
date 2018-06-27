ff2dfb01bf1f447e1938a59d4c589d9803e11c36
Dylan Richardson <dylanjackrichardson@gmail.com>
2018-06-27 14:31:00 -0500

    First commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import SQL from "sql.js";
 
-Vue.config.productionTip = false
+let obj = {
+  db: new Promise((res, rej) => {
+    function wasm(SQL) {
+      const db = new SQL.Database();
+      res(db);
+    }
+    require("spatiasql")(wasm);
+  })
+};
+
+obj.db.then(db => {
+  console.log(db.exec("select sqlite_version()")[0].values[0]);
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
